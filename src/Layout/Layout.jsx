c0fa3f7d0a1058b1c0674431d1bf6955ef345a59
Nafiz Al Turabi <nafizalturabi18cse@gmail.com>
2024-11-03 17:05:45 +0600

    updated

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -1,6 +1,6 @@
 // Layout.js
 import React, { useState } from 'react';
-import { Outlet, Link } from 'react-router-dom';
+import { Outlet, Link, NavLink } from 'react-router-dom';
 import { Menu, X, ChevronDown, Search } from 'lucide-react';
 
 const Layout = () => {
@@ -46,7 +46,7 @@ const Layout = () => {
                         >
                             {sidebarOpen ? <X size={20} /> : <Menu size={20} />}
                         </button>
-                        <span className="text-xl font-bold">Your Library</span>
+                        <span className="text-xl font-bold uppercase">NAT UI Library</span>
                     </div>
                     <div className="hidden md:flex relative w-96">
                         <Search className="absolute left-3 top-2.5 h-4 w-4 text-gray-500" />
@@ -73,15 +73,18 @@ const Layout = () => {
                                 <h2 className="font-semibold text-gray-900">{section.title}</h2>
                                 <ChevronDown size={16} className="text-gray-500" />
                             </div>
-                            <ul className="">
+                            <ul>
                                 {section.items.map((item, itemIdx) => (
                                     <li key={itemIdx}>
-                                        <Link
-                                            to={item.path}  // Use the path defined in navigation
-                                            className="block border-l px-3 py-2 text-sm text-gray-600 hover:bg-gray-100 hover:text-gray-900"
+                                        <NavLink
+                                            to={item.path}
+                                            className={({ isActive }) =>
+                                                `block border-l px-3 py-2 text-sm ${isActive ? 'p-text font-semibold border-blue-600 ' : 'text-gray-600 hover:text-[#c44221]'
+                                                }`
+                                            }
                                         >
                                             {item.name}
-                                        </Link>
+                                        </NavLink>
                                     </li>
                                 ))}
                             </ul>
@@ -92,7 +95,7 @@ const Layout = () => {
 
             {/* Main Content */}
             <main className={`pt-16 ${sidebarOpen ? 'ml-64' : 'ml-0'} min-h-screen transition-all duration-200 ease-in-out`}>
-                <div className="container mx-auto p-6">
+                <div className="container mx-auto p-6 bg-red-200">
                     <Outlet /> {/* Renders nested route components here */}
                 </div>
             </main>
